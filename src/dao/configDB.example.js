609a4cd6a912b853d217564600a4e668d1927a90
Guilherme Delazeri <delazeri1997@gmail.com>
2018-09-18 20:31:45 -0300

    Added configDb that stores database configurations

--- src/dao/configDB.example.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    user: 'username',
-    host: 'endpoint',
-    database: 'databaseName',
-    password: 'databasePassword',
-    port: 3000,
-    ssl: true,
-};
---
+++ src/dao/configDB.js
@@ -0,0 +1,8 @@
+module.exports = {
+    user: 'jhillkrxylkaud',
+    host: 'ec2-174-129-18-98.compute-1.amazonaws.com',
+    database: 'dd2sc4an42h052',
+    password: '7c230617bbe30c5ff27dbf515e05c9910021c25819ff8847beebd777ab70e27c',
+    port: 5432,
+    ssl: true,
+};
