fba1499565e7c9ea6845db69e205d8b48c6b592d
Guilherme Delazeri <delazeri1997@gmail.com>
2018-10-27 13:55:52 -0300

    Added comments in professional routes

--- src/routes/client.js
+++ src/routes/client.js
@@ -9,10 +9,13 @@ router.get('/list/:professionalId?', ClientController.list);
 /* GET /client/get/{id} */
 router.get('/get/:id', ClientController.get);
 
+/* POST /client/add/{id} */
 router.post('/add', ClientController.add);
 
+/* PUT /client/edit/{id} */
 router.put('/edit/:id', ClientController.edit);
 
+/* DELETE /client/delete/{id} */
 router.delete('/delete/:id', ClientController.delete);
 
 module.exports = router;
--- src/routes/professional.js
+++ src/routes/professional.js
@@ -3,16 +3,19 @@ const ProfessionalController = require('../controllers/professionalController');
 
 const router = express.Router();
 
-/* GET /professional/list/{professionalId} */
-router.get('/list/:professionalId?', ProfessionalController.list);
+/* GET /professional/list */
+router.get('/list', ProfessionalController.list);
 
 /* GET /professional/get/{id} */
 router.get('/get/:id', ProfessionalController.get);
 
+/* POST /professional/add */
 router.post('/add', ProfessionalController.add);
 
+/* PUT /professional/edit/{id} */
 router.put('/edit/:id', ProfessionalController.edit);
 
+/* DELETE /professional/delete/{id} */
 router.delete('/delete/:id', ProfessionalController.delete);
 
 module.exports = router;
